b3f1304765f57c3a2bc7a64a985129a6cdb05829
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-01-04 11:12:33 -0700

    initial alcoholic checkbox setup, length limiter, and name generation

--- assets/js/cocktailsApi.js
+++ assets/js/cocktailsApi.js
@@ -1,21 +1,64 @@
+//////////////////////
+// Element Selectors//
+//////////////////////
+let alcCheckEl = $('#alcCheck')
+let runEl = $('#run')
+let queryLengthEl = $('#queryLength')
+let allDrinksEl = $('#allDrinks')
 
-var alc = "Non_Alcoholic";
-var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=" + alc;
+////////////////
+// API Queries//
+////////////////
+var alc = "Alcoholic";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-    for (var i = 0; i < response.length; i++) {
-        let drinkName = response.drinks[i].strDrink
-        console.log(response.drinks[0].strDrink)
+
+runEl.click(function (){
+    apiCall()
+})
+alcCheckEl.click(function (){
+
+    if (alcCheckEl.prop('checked')) {
+        console.log("Display Non Alc only")
+        alc = "Non_Alcoholic"
     }
-    // $.each(response, function( index, value ) {
-    //     console.log(response.drinks[0].strDrink)
-    // });
-    // console.log(response.drinks[i].strDrink)
-}) 
+    else {
+        console.log("Display All")
+        alc = "Alcoholic"
+    }
+})
 
+let apiCall = function () {
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=" + alc;
+    let length = queryLengthEl.val()
+    console.log('apicall')
+    console.log(queryURL)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        // console.log(response);
+        console.log(length)
+        allDrinksEl.empty();
+
+        for (var i = 0; i < length; i++) {
+            //declare specific response values
+            let drinkName = response.drinks[i].strDrink
+
+            //create html elements for each response
+            let newDrink = $('<div>');
+            let drinkNameEl = $('<h3>')
+            
+            //set element values
+            drinkNameEl.text(drinkName);
+            allDrinksEl.append(newDrink);
+            newDrink.append(drinkName)
+    
+
+
+        }
+    }) 
+    
+        
+}
 
 
